91212ce4b332d1332e2e3fb3166eaac51223565c
MaxwellMuF <sebastian.rohrscheidt@outlook.com>
2025-01-28 23:46:07 +0100

    update icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,18 +15,18 @@ def init_st_session_state():
 def pages_bevor_login():
     """Pages of streamlit app bevor login defined by functions"""
     login = st.Page(methods_login.login_widget, title="Login", icon=":material/home:")
-    register_new_user = st.Page(methods_login.register_new_user_widget, title="Reset Password", icon=":material/key:")
-    forgot_password = st.Page(methods_login.forgot_password_widget, title="Forget Password", icon=":material/key:")
-    forgot_username = st.Page(methods_login.forgot_username_widget, title="Forget Username", icon=":material/key:")
+    register_new_user = st.Page(methods_login.register_new_user_widget, title="Sign Up", icon=":material/person_add:")
+    forgot_password = st.Page(methods_login.forgot_password_widget, title="Forget Password", icon=":material/lock_reset:")
+    forgot_username = st.Page(methods_login.forgot_username_widget, title="Forget Username", icon=":material/help_outline:")
 
     return [login, register_new_user, forgot_password, forgot_username]
 
 def pages_after_login():
     """Pages of streamlit app after login defined by functions and python files"""
     welcome = st.Page("application/src/ui/streamlit_pages/page_1_welcome.py", title="Welcome", icon=":material/home:")
-    raw_data = st.Page("application/src/ui/streamlit_pages/page_2_raw_data.py", title="Raw data", icon=":material/home:")
-    haushalt = st.Page("application/src/ui/streamlit_pages/page_3_12years.py", title="12 years Bundeshaushalt", icon=":material/home:", default=True)
-    reset_password = st.Page(methods_login.reset_password_widget, title="Reset Password", icon=":material/key:")
+    raw_data = st.Page("application/src/ui/streamlit_pages/page_2_raw_data.py", title="Raw data", icon=":material/dataset:")
+    haushalt = st.Page("application/src/ui/streamlit_pages/page_3_12years.py", title="12 years Bundeshaushalt", icon=":material/monitoring:")
+    reset_password = st.Page(methods_login.reset_password_widget, title="Reset Password", icon=":material/lock_reset:")
     logout = st.Page(methods_login.logout_widget, title="Logout", icon=":material/home:")
 
     return [welcome, raw_data, haushalt, reset_password, logout]
